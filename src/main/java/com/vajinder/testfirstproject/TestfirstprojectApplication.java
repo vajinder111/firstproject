67840c11d930446a9c27909bcdc82e4979f2c769
vajinder111 <138351573+vajinder111@users.noreply.github.com>
2023-11-25 17:21:06 +0530

    Initial Commit

---
+++ src/main/java/com/vajinder/testfirstproject/TestfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.vajinder.testfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfirstprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vajinder/testfirstproject/TestfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vajinder.testfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
